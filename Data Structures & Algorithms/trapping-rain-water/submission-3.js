0ed41c62ae9a1c23f1d2f7ce3502ba5a036b2020
Harshit Kandpal <hkandpal944@gmail.com>
2026-05-29 15:27:38 -0400

    Add: trapping-rain-water - submission-3

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-3.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     */
+    trap(height) {
+        let water = 0;
+        let lMax = height[0], rMax = height[height.length - 1];
+        let l = 0, r = height.length - 1;
+
+        while (l < r) {
+            if (lMax < rMax) {
+                lMax = Math.max(lMax, height[++l]);
+                water += lMax - height[l];
+            } else {
+                rMax = Math.max(rMax, height[--r]);
+                water += rMax - height[r];
+            }
+        }
+        return water;
+    }
+}
